da256a3db1e6db887db84611d59727a73f96135b
Kyle Fuson <kfuson123@gmail.com>
2017-09-18 14:02:38 -0400

    Started talk of game rules

--- War/main.cpp
+++ War/main.cpp
@@ -29,9 +29,105 @@ bool card_greater(Card a, Card b) {
 }
 
 
+// Deck of cards
+// -which cards? suited cards(normal cards). Jokers??
+// -how many cards? how many decks?
+// -???
+
+// Players
+// -How many players? usually 2?
+// -No strategy
+// -Per player accquisiton strategy......
+// -names?
+// -hand (queue of cards)
+
+// Game Option
+// -deck specification/size
+// -aces low or high
+// -accquistion of cards (spoils)
+//  -winner's first
+// 	-loser's first
+// 	-sorted
+// 	-shuffled
+// 	-interspersed per war
+// -number of sacrifices (1, 3, ?)
+// -negotiable sacrifice? (possibly 0)
+// -incremental sacrifice?
+// 	-global or local counter?
+
 int main()
 {
 	prng.seed(rng());
+	// Game deck
+	Deck deck;
+	deck.shuffle();
+
+	Player p1;
+	Player p2;
+
+	// Splits the deck
+	deal(deck, p1, deck.size()/2);
+	deal(deck, p2, deck.size());
+
+	// Round robin
+	while(!deck.empty())
+	{
+		deal_one(deck, p1);
+		deal_one(deck, p2);
+	}
+
+	// Very object oriented
+	// Creates an artificial dependency from deck to player and limit reusability
+	// deck.deal(deck, p1, deck.size()/2);
+	// deck.deal(deck, p2, deck.size());
+
+	Pile spoils;
+	
+	while(true)
+	{
+
+		if(p1.empty())
+		{
+			if(p2.empty())
+				std::cout << "TIE\n";
+			
+			else
+				std::cout << "P2 WINS\n";
+			break;
+		}
+		else if(p2.empty())
+		{
+			std::cout << "P1 WINS\n";
+			break;
+		}
+
+
+		Card c1 = p1.take(); 
+		Card c2 = p2.take();
+
+		spoils.add(c1);
+		spoils.add(c2);
+
+		if(c1 > c2)
+		{
+			give(p1, spoils);
+		}
+		else if (c2 > c1)
+		{
+			give(p2, spoils);
+		}
+		else
+		{
+			// WAR
+			spoils.add(p1.take());
+			spoils.add(p2.take());
+			continue;
+		}
+		assert(spoils.empty());
+	}
+
+
+
 
 
 	std::vector<Card> deck;
@@ -51,7 +147,7 @@ int main()
 
 
 
-    //std::sort(deck.begin(), deck.end());
+    std::sort(deck.begin(), deck.end());
 
     // Rank r1 = Two;
     // Rank r2 = Ace;
