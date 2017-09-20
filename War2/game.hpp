50b403ffb81452b3ae1782d2400fbaaf45392227
Kyle Fuson <kfuson123@gmail.com>
2017-09-20 13:32:25 -0400

    Created game files

---
+++ War2/game.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+
+struct Options
+{
+	bool ace_high = true;
+	int num_sacrifice = 1;
+	bool negootiable_sacrifice = true;
+	int num_decks = 1;
+};
+struct Game
+{
+	Deck deck;
+	Player p1;
+	Player p2;
+	Pile pile;
+	int turn;
+};
