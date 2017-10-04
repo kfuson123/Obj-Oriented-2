79d66c9120d0ea908eba52c82e3cb01d4b73652a
Kyle Fuson <31484695+kfuson123@users.noreply.github.com>
2017-10-04 13:48:06 -0400

    Add files via upload

---
+++ Blackjack/card.hpp
@@ -0,0 +1,67 @@
+
+#pragma once
+
+#include <utility>
+
+#include <iosfwd>
+
+enum Rank
+{
+  Ace = 1,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack = 10,
+  Queen = 10,
+  King = 10,
+};
+
+enum Suit
+{
+  Hearts,
+  Diamonds,
+  Clubs,
+  Spades,
+};
+
+/// Represents the values of (suited) cards.
+class Card
+{
+public:
+  Card() = default;
+
+  Card(Rank r, Suit s)
+    : rank(r), suit(s) // member initializer list
+  { }
+
+  // Returns the rank of the card.
+  Rank get_rank() const { return rank; }
+
+  // Returns the suit of the card.
+  Suit get_suit() const { return suit; }
+
+private:
+  Rank rank;
+  Suit suit;
+};
+
+// Equality comparison
+bool operator==(Card a, Card b);
+bool operator!=(Card a, Card b);
+
+// Ordering
+bool operator<(Card a, Card b);
+bool operator>(Card a, Card b);
+bool operator<=(Card a, Card b);
+bool operator>=(Card a, Card b);
+
+std::ostream& operator<<(std::ostream& os, Card c);
+std::ostream& operator<<(std::ostream& os, Rank r);
+std::ostream& operator<<(std::ostream& os, Suit s);
+
---
+++ Blackjack/game.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "deck.hpp"
+#include "card.hpp"
+
+void Split(Deck& deck, Hand& player, Hand& split_hand);
+
+
+struct Currency
+{
+	
+}
+
